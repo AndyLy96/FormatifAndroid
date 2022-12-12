7c94c80d6c581a6f6c859e6fc6e736b0755d6ea4
Andy <2157338@cegepmontpetit.ca>
2022-12-12 14:07:11 -0500

    QUESTION 1 FINI

---
+++ Interface Graphique - Q1/app/src/main/java/com/example/navigation/MainActivity2.java
@@ -0,0 +1,22 @@
+package com.example.navigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.navigation.databinding.ActivityMain2Binding;
+import com.example.navigation.databinding.ActivityMainBinding;
+
+public class MainActivity2 extends AppCompatActivity {
+    ActivityMain2Binding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMain2Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
---
+++ Interface Graphique - Q1/app/src/main/java/com/example/navigation/Page_A.java
@@ -0,0 +1,37 @@
+package com.example.navigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.navigation.databinding.ActivityMainBinding;
+
+public class Page_A extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.article.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Page_A.this, "yo", Toast.LENGTH_SHORT).show();
+
+                Intent i = new Intent(getApplicationContext(), MainActivity2.class);
+                startActivity(i);
+            }
+        });
+
+
+
+
+
+    }
+}
